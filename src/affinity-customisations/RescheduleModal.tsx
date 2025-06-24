ab5261d09db1ea4bb9ae18ce60dc931a9858027f
James Cartwright <james.cartwright@me.com>
2025-06-24 10:04:51 +0100

    Convert to react components

---
+++ src/affinity-customisations/RescheduleModal.tsx
@@ -0,0 +1,45 @@
+import { Description, Dialog, DialogPanel, DialogTitle, DialogBackdrop, CloseButton } from '@headlessui/react';
+import { useState } from 'react';
+
+export function RescheduleModal() {
+  let [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setIsOpen(true)}>Reschedule</button>
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50 ">
+        <DialogBackdrop transition className="affinity-modal-overlay fixed inset-0 bg-black/30" />
+
+        <div className="recharge-theme fixed inset-0 flex w-screen items-end lg:items-center justify-center p-">
+          <DialogPanel
+            transition
+            className="affinity-modal-panel max-w-lg space-y-4 bg-white p-12 transition-all duration-300 data-[closed]:opacity-0 data-[closed]:translate-y-5"
+          >
+            <div className="flex justify-end mb-0 sticky">
+              <CloseButton className="affinity-modal-close">
+                <svg viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" width="14" height="14">
+                  <path
+                    d="m1 1 12 12M1 13 13 1"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                </svg>
+              </CloseButton>
+            </div>
+            <div className="mt-[-32px]">
+              <DialogTitle className="font-bold mt-0">Deactivate account</DialogTitle>
+              <Description>This will permanently deactivate your account</Description>
+              <p>Are you sure you want to deactivate your account? All of your data will be permanently removed.</p>
+              <div className="flex gap-4">
+                <button onClick={() => setIsOpen(false)}>Cancel</button>
+                <button onClick={() => setIsOpen(false)}>Deactivate</button>
+              </div>
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+    </>
+  );
+}
