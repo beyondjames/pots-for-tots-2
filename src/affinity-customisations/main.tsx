e95cda4a0140dd48024cca9322eb85e514891f21
James Cartwright <james.cartwright@me.com>
2025-06-25 17:30:03 +0100

    Remove old vite code

--- src/affinity-customisations/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { AffinityCustomisations } from './AffinityCustomisations';
-
-createRoot(document.getElementById('affinity-customisations')!).render(
-  <StrictMode>
-    <AffinityCustomisations />
-  </StrictMode>,
-);
