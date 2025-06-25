c3c5f3748c568fb0b74d41ebff2afc23512b7ea6
James Cartwright <james.cartwright@me.com>
2025-06-25 17:30:21 +0100

    Updated widget to update existing widget

--- assets/affinity-extensions.js
+++ assets/affinity-extensions.js
@@ -1,14 +1,10 @@
-// Initialize Recharge.js
 recharge.init({
   storeIdentifier: 'pots-for-tots-food.myshopify.com', // On Shopify this should be your myshopify.com domain
-  // required for Storefront API access
   storefrontAccessToken: 'strfnt_c37d4459c60023c6fa298076cebb74e202536004d8118c7da0009160085645d4',
   appName: 'Pots for Tots Theme',
   appVersion: '1.0.0',
   loginRetryFn: () => {
     return recharge.auth.loginShopifyAppProxy().then((session) => {
-      // do anything you want with the session here
-      // return session
       return session;
     });
   },
@@ -54,37 +50,36 @@ const rechargeAPI = {
     return this.session;
   },
 
-  async fetchSubscriptions() {
-    const response = await recharge.subscription
-      .listSubscriptions(this.session, {
-        sort_by: 'id-asc',
-        status: 'Active',
-      })
-      .catch((error) => {
-        console.error('Fetching subscriptions failed:', error);
-        throw error;
-      });
-    console.log('Fetched subscriptions:', response.subscriptions);
-    return response.subscriptions;
-  },
+  // async fetchSubscriptions() {
+  //   const response = await recharge.subscription
+  //     .listSubscriptions(this.session, {
+  //       sort_by: 'id-asc',
+  //       status: 'Active',
+  //     })
+  //     .catch((error) => {
+  //       console.error('Fetching subscriptions failed:', error);
+  //       throw error;
+  //     });
+  //   return response.subscriptions;
+  // },
 
-  async fetchOrders() {
-    const response = await recharge.order
-      .listOrders(this.session, {
-        limit: 10,
-        sort_by: 'id-asc',
-      })
-      .catch((error) => {
-        console.error('Fetching orders failed:', error);
-        throw error;
-      });
-    return response.orders;
-  },
+  // async fetchOrders() {
+  //   const response = await recharge.order
+  //     .listOrders(this.session, {
+  //       limit: 10,
+  //       sort_by: 'id-asc',
+  //     })
+  //     .catch((error) => {
+  //       console.error('Fetching orders failed:', error);
+  //       throw error;
+  //     });
+  //   return response.orders;
+  // },
 
   async fetchCharges() {
     const response = await recharge.charge
       .listCharges(this.session, {
-        limit: 20,
+        limit: 25,
         sort_by: 'id-asc',
       })
       .catch((error) => {
@@ -101,7 +96,6 @@ const rechargeAPI = {
         console.error('Updating next charge date failed:', error);
         throw error;
       });
-    console.log('Updated next charge date:', response);
     return response;
   },
 };
